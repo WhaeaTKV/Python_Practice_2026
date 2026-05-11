f7620d8c82cc732af889d6bebb840b1c76ad5d51
WhaeaTKV <kapa-viviant@wsc.school.nz>
2026-05-12 11:36:18 +1200

    update new files

---
+++ Lesson 4 - If/packing_check_list.py
@@ -0,0 +1,31 @@
+### Create a packing checklist based on input
+
+# Tell the user what this program is about
+
+# Ask the user for the current temperature and save it
+
+# Change the temperature input into an integer
+
+# If the temperature is below 15, tell them to pack a jacket
+
+# If the temperature is above 15, tell them to pack sunscreen
+
+# Ask the user their destination (beach or mountains)
+
+# If beach, tell them to pack a towel
+
+# If mountains, tell them to pack hiking boots
+
+# ___________________________
+
+# EXTENSION
+
+# Add some more conditions (eg. one day or overnight? solo or with others?)
+
+# ____________________________
+
+# EXPERT (for those who already know Python)
+
+# Create a packing checklist (start with something similar to the main program) then 
+# display all items to pack with a X or O for packed or not. 
+# Allow the user to select an item to change its status.
